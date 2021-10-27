2aba12c90855f2d9ffba67671f147673385a6f7b
unknown <80782387+aalister@users.noreply.github.com>
2021-10-27 10:13:53 +0100

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,9 +3,14 @@
 
 const pages = [
 	{% for p in site.html_pages %}
+	{% assign lines = p.content | newline_to_br | split: "<br />" %}
 	{
 		title: "{{ p.title }}",
-		content: "{{ p.content | newline_to_br | split "<br />" }}"
+		headers: [
+			{% for line in lines %}
+			"{{ line }}",
+			{% endfor %}
+		]
 	},
 	{% endfor %}
 ];
