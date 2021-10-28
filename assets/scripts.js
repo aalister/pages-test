cb63c9ce8efed2d9655207e6a1c5ad2356ace7c8
unknown <80782387+aalister@users.noreply.github.com>
2021-10-28 19:13:17 +0100

    Update Search

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,13 +1,18 @@
----
----
-
 const pages = [
-	{% for p in site.html_pages %}
+	{%- for p in site.html_pages %}
 	{
+		url: "{{ p.url }}",
 		title: "{{ p.title }}",
-		content: "{{ p.content | newline_to_br | split: "<br />" }}"
+		headers: [
+			{%- assign headers = p.content | markdownify | split: "<h" | shift %}
+			{%- for header in headers %}
+			{%- assign level = header | slice: 0 %}
+			{%- assign content = header | split: "</h" | first | split: ">" | last %}
+			{ level: {{ level }}, content: "{{ content }}" },
+			{%- endfor %}
+		]
 	},
-	{% endfor %}
+	{%- endfor %}
 ];
 
 (function() {
