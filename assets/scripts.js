64619e85c926950886070750064c674ea9be04fa
unknown <80782387+aalister@users.noreply.github.com>
2021-10-28 19:25:10 +0100

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,14 +2,14 @@
 ---
 const pages = [
 	{%- for p in site.html_pages %}
-	{ url: "{{ p.url }}", title: "{{ p.title }}", headers: [
+	{url: "{{ p.url }}", title: "{{ p.title }}", headers: [
 		{%- assign headers = p.content | markdownify | split: "<h" | shift %}
 		{%- for header in headers %}
 			{%- assign level = header | slice: 0 %}
 			{%- assign content = header | split: "</h" | first | split: ">" | last -%}
-			{ level: {{ level }}, content: "{{ content }}" },
-		{%- endfor %}
-		] },
+			{level: {{ level }}, content: "{{ content }}"},
+		{%- endfor -%}
+		]},
 	{%- endfor %}
 ];
 
