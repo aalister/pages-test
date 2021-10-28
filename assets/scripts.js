5238bbf5c0c23b1b890fd5f872fb407b5130bff3
unknown <80782387+aalister@users.noreply.github.com>
2021-10-28 19:47:41 +0100

    Testing quotation marks

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,12 +2,12 @@
 ---
 const pages = [
 	{%- for p in site.html_pages %}
-	{url: "{{ p.url }}", title: "{{ p.title }}", headers: [
+	{url: "{{ p.url }}", title: "{{ p.title | replace: '"', "'" }}", headers: [
 		{%- assign headers = p.content | markdownify | split: "<h" | shift %}
 		{%- for header in headers %}
 			{%- assign level = header | slice: 0 %}
 			{%- assign content = header | split: "</h" | first | split: ">" | last -%}
-			{level: {{ level }}, content: "{{ content }}"},
+			{level: {{ level }}, content: "{{ content | replace: '"', "'" }}"},
 		{%- endfor -%}
 		]},
 	{%- endfor %}
--- subfolder/test.md
+++ subfolder/test.md
@@ -1,4 +1,4 @@
-# Heading 1
+# Heading "1"
 
 Paragraph 1.
 
