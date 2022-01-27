d0e2d168259cf80e54eac591fe640a48793aff0d
unknown <80782387+aalister@users.noreply.github.com>
2022-01-27 18:57:16 +0000

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,3 @@
----
----
-const pages = [
-	{%- for p in site.html_pages %}
-	{url: "{{ p.url | relative_url }}", title: "{{ p.title | replace: '"', "'" }}", headers: [
-		{%- assign headers = p.content | markdownify | split: "<h" | shift %}
-		{%- for header in headers %}
-			{%- assign header = header | split: "</h" | first %}
-			{%- assign level = header | slice: 0 %}
-			{%- assign content = header | split: ">" | last -%}
-			{level: {{ level }}, id: "{{ id }}", content: "{{ content | replace: '"', "'" }}"},
-		{%- endfor -%}
-		]},
-	{%- endfor %}
-];
-
 (function() {
 	console.log("hello world");
 })();
